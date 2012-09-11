1fe5e15c1f3eaad2fa40fc83539c0ddca1d5be6d
4B796C65@gmail.com <4B796C65@gmail.com@dc44cbd8-c6c8-7236-b2bb-41d4121a621a>
2012-09-11 05:41:05 +0000

    Started work on unit testing.  But a bug prevents me from finishing it.

---
+++ unittest/runTest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+import unittest
+import time
+import random
+import atexit
+atexit.register(lambda: time.sleep(3))
+
+sys.path.insert(0, '..')
+import tdl
+
+class SimpleConsoleTest(unittest.TestCase):
+
+    def setUp(self):
+        self.console = tdl.init(30, 20, 'TDL UnitTest')
+
+    def tearDown(self):
+        del self.console
+
+class DrawCharTest(SimpleConsoleTest):
+
+    def test_dcTuples(self):
+        self.console.clear()
+        ch = (1, (255, 255, 255), (0, 0, 0))
+        self.console.drawChar(0, 0, *ch)
+        tdl.flush()
+        # a critcal error with getChar prevents testing, I'll need to update libtcod
+        self.assertEqual(ch, self.console.getChar(0, 0), 'err?')
+        
+def suite():
+    loader = unittest.TestLoader()
+    return unittest.TestSuite([loader.loadTestsFromTestCase(DrawCharTest)])
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
+
