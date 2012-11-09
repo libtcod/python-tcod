0f31499282a071cc457db7ddd7297b0030997f27
4b796c65 <4b796c65@dc44cbd8-c6c8-7236-b2bb-41d4121a621a>
2012-11-09 04:42:56 +0000

    Added libtcod pathfinding.  Untested.

--- trunk/tdl/fov.py
+++ trunk/tdl/fov.py
@@ -1,22 +1,40 @@
 
 import itertools
+import ctypes
+import array
 
 from .__tcod import _lib
 
 class Map(object):
     
-    def __init__(self, width, height):
+    def __init__(self, width, height, callback):
         self._width = int(width)
         self._height = int(height)
         self._size = self._width * self._height
         self._tcodMap = _lib.TCOD_map_new(width, height)
         self._as_parameter_ = self._tcodMap
-        self._fovConf = (0, 0, 0) # fov settings (x, y, radius)
+        self._callback = callback
+        self._clean = set()
         #self._walkable = array.array('b', [0] * self._size)
         #self._transparent = array.array('b', [0] * self._size)
         
     def __del__(self):
         _lib.TCOD_map_delete(self)
+        
+    def _pointsInRadius(self, x, y, radius):
+        'returns a list of (x, y) items'
+        x = range(max(0, x - radius), min(x + radius + 1, self._width))
+        y = range(max(0, y - radius), min(y + radius + 1, self._height))
+        return itertools.product(x, y)
+    
+    def _pointsInRadiusC(self, x, y, radius):
+        'returns a list of ((x, ctypeX), (y, ctypeY)) items'
+        c_int = ctypes.c_int
+        x = ((i, c_int(i)) for i in
+             range(max(0, x - radius), min(x + radius + 1, self._width)))
+        y = ((i, c_int(i)) for i in
+             range(max(0, y - radius), min(y + radius + 1, self._height)))
+        return itertools.product(x, y)
         
     def setFromCallbacks(self, walkableCall, transparentCall):
         for x, y in itertools.product(range(self._width), range(self._height)):
@@ -30,6 +48,15 @@ class Map(object):
         _lib.TCOD_map_set_properties(self._as_parameter_,
                                      x, y, walkable, transparent)
         
+    def _updateMap(self, x, y, radius):
+        c_bool = ctypes.c_bool
+        for (x, cX),(y, cY) in self._pointsInRadiusC(x, y, radius):
+            #if (x, y) not in self._clean:
+            #    self._clean.add((x,y))
+                transparent = c_bool(self._callback(x, y))
+                _lib.TCOD_map_set_properties(self._as_parameter_,
+                                         cX, cY, transparent, transparent)
+        
     def computeFOV(self, x, y, fov='PERMISSIVE', radius=8, lightWalls=True):
         oldFOV = fov
         fov = str(fov).upper()
@@ -41,15 +68,19 @@ class Map(object):
             fov = 4 + int(fov[10])
         else:
             raise TDLError('No such fov as %s' % oldFOV)
-        _lib.TCOD_map_compute_fov(self, x, y, radius, lightWalls, fov)
-        self._fovConf = (x, y, radius)
-        return self._iterFOV()
             
-    def _iterFOV(self):
-        x, y, radius = self._fovConf
+        self._updateMap(x, y, radius)
+        _lib.TCOD_map_compute_fov(self, x, y, radius, lightWalls, fov)
+        return self._listFOV(x, y, radius)
+            
+    def _iterFOV(self, x, y, radius):
         inFOV = _lib.TCOD_map_is_in_fov
         map = self._as_parameter_
-        for x,y in itertools.product(range(x - radius, x + radius + 1),
-                                     range(y - radius, y + radius + 1)):
-            if inFOV(map, x, y):
+        for (x, cX),(y, cY) in self._pointsInRadiusC(x, y, radius):
+            if inFOV(map, cX, cY):
                 yield(x, y)
+                
+    def _listFOV(self, x, y, radius):
+        return list(self._iterFOV(x, y, radius))
+
+__all__ = [Map]
