77b91d922cdb38b30c8b963293789c6241a7357a
4B796C65@gmail.com <4B796C65@gmail.com@dc44cbd8-c6c8-7236-b2bb-41d4121a621a>
2012-12-06 05:02:36 +0000

    Tag release 1.1.1

---
+++ tags/python-tdl-1.1.1/examples/helloWorld.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+    Example showing some of the most basic functions needed to say "Hello World"
+"""
+# you can skip past this part and go onto the next.
+# what this does is allow tdl to be imported without installing it first.
+import sys
+sys.path.insert(0, '../')
+
+# import the tdl library and all of it's functions, this gives us access to anything
+# starting with "tdl."
+import tdl
+
+# start the main console, this will open the window that you see and give you a Console.
+# we make a small window that's 20 tiles wide and 16 tile's tall for this example.
+console = tdl.init(20, 16)
+
+# draw the string "Hello World" at the top left corner using the default colors:
+# a white forground on a black background.
+console.drawStr(0, 0, 'Hello World')
+
+# display the changes to the console with flush.
+# if you forget this part the screen will stay black emptiness forever.
+tdl.flush()
+
+# wait for a key press, any key pressed now will cause the program flow to the next part
+# which closes out of the program.
+tdl.event.keyWait()
+
+# if you run this example in IDLE then we'll need to delete the console manually
+# otherwise IDLE prevents the window from closing causing it to hang
+del console
---
+++ tags/python-tdl-1.1.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os.path
+import subprocess
+from distutils.core import setup
+
+setup(name='tdl',
+      version='1.1.1',
+      author='Kyle Stewart',
+      author_email='4B796C65+pythonTDL@gmail.com',
+      description='Simple graphical library for making a roguelike or other tile-based video game.',
+      long_description="""python-tdl is a ctypes port of "libtcod".
+
+      The library is used for displaying tilesets (ascii or graphical) in true color.
+      """,
+      url='http://code.google.com/p/python-tdl/',
+      download_url='http://code.google.com/p/python-tdl/downloads/list',
+      packages=['tdl'],
+      package_data={'tdl': ['lib/*.txt', '*.bmp', '*.png', 'lib/win32/*',
+                            'lib/darwin/*.dylib', 'lib/linux*/*']},
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Win32 (MS Windows)',
+                   'Environment :: MacOS X',
+                   'Environment :: X11 Applications',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX',
+                   'Operating System :: MacOS',
+                   'Operating System :: Microsoft :: Windows',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: Implementation :: PyPy',
+                   'Topic :: Games/Entertainment',
+                   'Topic :: Multimedia :: Graphics',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+      keywords = 'roguelike roguelikes console text ctypes doryen ascii libtcod',
+      platforms = ['Windows', 'Mac OS X', 'Linux'],
+      license = 'New BSD License'
+      )
