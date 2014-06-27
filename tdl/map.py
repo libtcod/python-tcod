42a2a7753ec05bc3b1853ade16de0c0a465de052
4b796c65 <4b796c65@dc44cbd8-c6c8-7236-b2bb-41d4121a621a>
2014-06-27 09:30:22 +0000

    Added bresenham

--- tdl/map.py
+++ tdl/map.py
@@ -192,38 +192,45 @@ def quickFOV(x, y, callback, fov='PERMISSIVE', radius=7.5, lightWalls=True, sphe
         _lib.TCOD_map_delete(tcodMap)
     return touched
     
-# def bresenham(x1, y1, x2, y2):
-    # points = []
-    # issteep = abs(y2-y1) > abs(x2-x1)
-    # if issteep:
-        # x1, y1 = y1, x1
-        # x2, y2 = y2, x2
-    # rev = False
-    # if x1 > x2:
-        # x1, x2 = x2, x1
-        # y1, y2 = y2, y1
-        # rev = True
-    # deltax = x2 - x1
-    # deltay = abs(y2-y1)
-    # error = int(deltax / 2)
-    # y = y1
-    # ystep = None
-    # if y1 < y2:
-        # ystep = 1
-    # else:
-        # ystep = -1
-    # for x in range(x1, x2 + 1):
-        # if issteep:
-            # points.append((y, x))
-        # else:
-            # points.append((x, y))
-        # error -= deltay
-        # if error < 0:
-            # y += ystep
-            # error += deltax
-    # # Reverse the list if the coordinates were reversed
-    # if rev:
-        # points.reverse()
-    # return points
+def bresenham(x1, y1, x2, y2):
+    """
+    Iterate over points in a bresenham line.
+    
+    Implementation hastily copied from RogueBasin.
+    
+    @return: Returns an iterator of (x, y) points.
+    """
+    points = []
+    issteep = abs(y2-y1) > abs(x2-x1)
+    if issteep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+    rev = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        rev = True
+    deltax = x2 - x1
+    deltay = abs(y2-y1)
+    error = int(deltax / 2)
+    y = y1
+    ystep = None
+    if y1 < y2:
+        ystep = 1
+    else:
+        ystep = -1
+    for x in range(x1, x2 + 1):
+        if issteep:
+            points.append((y, x))
+        else:
+            points.append((x, y))
+        error -= deltay
+        if error < 0:
+            y += ystep
+            error += deltax
+    # Reverse the list if the coordinates were reversed
+    if rev:
+        points.reverse()
+    return iter(points) # force as iter so I can sleep at night
     
 __all__ = ['AStar', 'quickFOV']
