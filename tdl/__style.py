2062ed64f06e2a94bbe5371ffb4df6b1885d6467
Kyle Stewart <4b796c65@gmail.com>
2015-04-03 23:29:50 -0700

    should probably document this

--- tdl/__style.py
+++ tdl/__style.py
@@ -1,8 +1,19 @@
+"""
+    Used internally to handle style changes without breaking backwards
+    compatibility.
+"""
 
 import warnings as _warnings
 import functools as _functools
 
 def backport(func):
+    """
+        Backport a function name into an old style for compatibility.
+        
+        The docstring is updated to reflect that the new function returned is
+        deprecated and that the other function is preferred.
+        A DeprecationWarning is also raised for using this function.
+    """
     @_functools.wraps(func)
     def deprecated_function(*args, **kargs):
         _warnings.warn('This finction name is deprecated',
