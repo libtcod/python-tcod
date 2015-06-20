683753cffae1a7d612283afaa2584f99f1ca620c
Kyle Stewart <4b796c65@gmail.com>
2015-06-20 07:17:06 -0700

    unused C++ header files removed

--- tdl/include/gui/vbox.hpp
+++
@@ -1,8 +0,0 @@
-class TCODLIB_GUI_API VBox : public Container {
-public :
-	VBox(int x, int y, int padding) : Container(x,y,0,0),padding(padding) {}
-	void computeSize();
-protected :
-	int padding;
-};
-
