dd83bf359be486584c5e7b31ee1bbb7abe22ba39
Kyle Stewart <4b796c65@gmail.com>
2015-06-21 16:01:15 -0700

    last rename, honest

--- tcod/tdl_source.c
+++ tcod/tdl_source.c
@@ -5,8 +5,8 @@
 // colors are converted to TCOD_color_t types in C and is much faster than in 
 // Python.
 // Also Python indexing is used, negative x/y will index to (width-x, etc.)
-int TDL_set_char(TCOD_console_t console, int x, int y,
-                 int ch, int fg, int bg, TCOD_bkgnd_flag_t blend){
+int TDL_console_put_char_ex(TCOD_console_t console, int x, int y,
+                            int ch, int fg, int bg, TCOD_bkgnd_flag_t blend){
     int width=TCOD_console_get_width(console);
     int height=TCOD_console_get_height(console);
     TCOD_color_t color;
