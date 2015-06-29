0cd46dc567330c2eeb6f3de44046d3aaf7d97755
Kyle Stewart <4b796c65@gmail.com>
2015-06-29 15:34:56 -0700

    parser error should have been only for listeners

--- tcod/parser.py
+++ tcod/parser.py
@@ -64,8 +64,8 @@ def new_struct(parser, name):
     return _lib.TCOD_parser_new_struct(parser, name)
 
 def run(parser, filename, listener=None):
-    raise NotImplementedError('The parser callbacks are not supported at this time.')
     if listener:
+        raise NotImplementedError('The listener callbacks are not supported at this time.')
         clistener = _ffi.new('TCOD_parser_listener_t *')
         def value_converter(name, typ, value):
             if typ == TYPE_BOOL:
