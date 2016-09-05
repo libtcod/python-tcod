0aa7ff6754c5b1cf00ccd9c064c1c5da37292876
Kyle Stewart <4b796c65@gmail.com>
2016-09-05 01:39:46 -0700

    set dynamic link path at compile

--- build_libtcod.py
+++ build_libtcod.py
@@ -38,6 +38,10 @@ extra_compile_args = []
 if sys.platform  in ['win32', 'win64', 'darwin']:
     include_dirs += ['tcod/includeSDL/']
 
+if 'linux' in sys.platform or 'darwin' in sys.platform:
+    extra_compile_args += ['-Wl', '-rpath=%s' %
+        os.path.join('$ORIGIN', _get_library_dirs_crossplatform())]
+
 ffi = FFI()
 ffi.cdef(open('tcod/libtcod_cdef.h', 'r').read())
 ffi.set_source(
--- tcod/libtcod.py
+++ tcod/libtcod.py
@@ -35,16 +35,6 @@ if 'win' in _sys.platform:
     _os.environ['PATH'] += ';' + _os.path.join(__path__[0],
                                                _get_lib_path_crossplatform())
 
-# add Mac dylib's to DYLD_LIBRARY_PATH
-if 'darwin' in _sys.platform:
-    _os.environ['DYLD_LIBRARY_PATH'] = _os.path.join(__path__[0],
-                                                _get_lib_path_crossplatform())
-
-_lib_ctypes = _ctypes.CDLL(
-    _os.path.realpath(
-        _os.path.join(__path__[0],
-            _get_lib_path_crossplatform(), _get_lib_name())))
-
 from . import _libtcod
 
 _ffi = ffi = _libtcod.ffi
