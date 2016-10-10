edbfa8e3f6c662d2f48653f95a1f88832a128742
Kyle Stewart <4b796c65@gmail.com>
2016-10-09 22:28:10 -0700

    add more tests

---
+++ tests/test_libtcodpy.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import unittest
+
+import tcod
+
+class TestLibtcodpyConsole(unittest.TestCase):
+
+    FONT_FILE = 'libtcod/terminal.png'
+    WIDTH = 12
+    HEIGHT = 12
+    TITLE = 'libtcod-cffi tests'
+    FULLSCREEN = False
+    RENDERER = tcod.RENDERER_SDL
+
+    def setUp(self):
+        tcod.console_set_custom_font(self.FONT_FILE)
+        self.console = tcod.console_init_root(self.WIDTH, self.HEIGHT,
+                                              self.TITLE, self.FULLSCREEN,
+                                              self.RENDERER)
+
+    def tearDown(self):
+        tcod.console_flush()
+        tcod.console_delete(self.console)
+
+    def test_console_info(self):
+        self.assertEqual(tcod.console_get_width(self.console), self.WIDTH)
+        self.assertEqual(tcod.console_get_height(self.console), self.HEIGHT)
+        self.assertEqual(tcod.console_is_fullscreen(), self.FULLSCREEN)
+        tcod.console_set_window_title(self.TITLE)
+        tcod.console_is_window_closed()
+
+
+class TestLibtcodpyLine(unittest.TestCase):
+    # arguments to test with and the results expected from these arguments
+    LINE_ARGS = (-5, 0, 5, 10)
+    EXCLUSIVE_RESULTS = [(-4, 1), (-3, 2), (-2, 3), (-1, 4), (0, 5), (1, 6),
+                         (2, 7), (3, 8), (4, 9), (5, 10)]
+    INCLUSIVE_RESULTS = [(-5, 0)] + EXCLUSIVE_RESULTS
+
+    def test_step(self):
+        ''' tcod.line_init and tcod.line_step
+        '''
+        tcod.line_init(*self.LINE_ARGS)
+        for expected_xy in self.EXCLUSIVE_RESULTS:
+            self.assertEqual(tcod.line_step(), expected_xy)
+        self.assertEqual(tcod.line_step(), (None, None))
+
+    def raise_error(*args):
+        raise Exception()
+
+    def test_line(self):
+        ''' tcod.line_line: normal, lazy, error
+
+        tests normal use, lazy evaluation, and error propagation
+        '''
+        # test normal results
+        test_result = []
+        def line_test(*test_xy):
+            test_result.append(test_xy)
+            return 1
+        self.assertEqual(tcod.line_line(*self.LINE_ARGS,
+                                        py_callback=line_test), 1)
+        self.assertEqual(test_result, self.INCLUSIVE_RESULTS)
+
+        # test lazy evaluation
+        test_result = []
+        def return_false(*test_xy):
+            test_result.append(test_xy)
+            return False
+        self.assertEqual(tcod.line_line(*self.LINE_ARGS,
+                                        py_callback=return_false), 0)
+        self.assertEqual(test_result, self.INCLUSIVE_RESULTS[:1])
+
+        # exception propagation
+        with self.assertRaises(Exception):
+            tcod.line_line(*LINE_ARGS, py_callback=self.raise_error)
+
+    def test_line_iter(self):
+        ''' tcod.line_iter
+        '''
+        self.assertEqual(list(tcod.line_iter(*self.LINE_ARGS)),
+                         self.EXCLUSIVE_RESULTS)
--- tests/test_testing.py
+++ tests/test_testing.py
@@ -8,7 +8,7 @@ curdir = os.path.dirname(__file__)
 
 FONT_FILE = os.path.join(curdir, 'data/fonts/consolas10x10_gs_tc.png')
 
-def test_console():
-    libtcod.console_set_custom_font(FONT_FILE, libtcod.FONT_LAYOUT_TCOD)
-    libtcod.console_init_root(40, 30, 'test', False, libtcod.RENDERER_SDL)
-    libtcod.console_flush()
+#def test_console():
+#    libtcod.console_set_custom_font(FONT_FILE, libtcod.FONT_LAYOUT_TCOD)
+#    libtcod.console_init_root(40, 30, 'test', False, libtcod.RENDERER_SDL)
+#    libtcod.console_flush()
