cbfb444a984d3c4c29e495ccc11e9f62309f8328
Kyle Stewart <4b796c65@gmail.com>
2018-03-09 18:46:41 -0800

    Made PyInstaller hook more reliable.
    [skip ci]

--- examples/distribution/PyInstaller/hook-tdl.py
+++ examples/distribution/PyInstaller/hook-tdl.py
@@ -1,3 +1,5 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
 hiddenimports = ['cffi']
 datas = collect_data_files('tdl') # packages the 'default' font
+# install all binaries to the working directory
+binaries = [(lib, '') for lib, _ in collect_dynamic_libs('tcod')]
