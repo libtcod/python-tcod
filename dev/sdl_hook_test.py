3d0e776da5fee5c4a14b34601216afd80642bbd1
Kyle Stewart <4b796c65@gmail.com>
2018-07-15 11:05:57 -0700

    Update SDL hook example.

--- dev/sdl_hook_test.py
+++ dev/sdl_hook_test.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python
 """
-    first draft example of libtcod's SDL hook
+    example of libtcod's SDL hook
 
-    draws an animated checkerboard pattern
+    draws a simple white square.
 """
 
 import tcod
 import tdl
 
+import numpy as np
+
 # generate a callback for libtcod
 @tcod.ffi.callback('SDL_renderer_t')
 def sdl_hook(surface):
-    # cast (void *) to (SDL_Surface *)
-    surface = tcod.ffi.cast('SDL_Surface *', surface)
+    tcod.lib.SDL_UpperBlit(my_surface, tcod.ffi.NULL, surface, [{'x':0, 'y':0}])
 
-    # assume pixels are 32bit, mostly unsafe, but this is what libtcod uses
-    pixels = tcod.ffi.cast('uint32*', surface.pixels)
+pixels = np.zeros((100, 150, 4), dtype=np.uint8)
+my_surface = tcod.lib.SDL_CreateRGBSurfaceWithFormatFrom(
+    tcod.ffi.cast('void*', pixels.ctypes.data),
+    pixels.shape[1], pixels.shape[0], 32,
+    pixels.strides[0],
+    tcod.lib.SDL_PIXELFORMAT_RGBA32,
+)
 
-    for y in range(surface.h):
-        for x in range(surface.w):
-            index = y * surface.w + x
-            # make a simple animatied pattern
-            bit = ((tick + x) % 100 < 50) ^ ((tick + y) % 100 < 50)
-            pixels[index] = 0xffffffff * bit
 
 if __name__ == '__main__':
     # hook callback to libtcod
     tcod.sys_register_SDL_renderer(sdl_hook)
 
     con = tdl.init(32, 32, renderer='SDL') # MUST BE SDL RENDERER
+
+    pixels[:] = 255
 
     tick = 0
     while(True):
