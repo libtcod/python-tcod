8f0dfe3946dc322979ad7549eaef7345a5129250
Kyle Stewart <4b796c65@gmail.com>
2019-01-25 16:13:28 -0800

    Use less ambiguous 'import libtcodpy' warning.

--- libtcodpy.py
+++ libtcodpy.py
@@ -1,4 +1,9 @@
 """This module just an alias for tcod"""
 import warnings
-warnings.warn("`import tcod` is preferred.", DeprecationWarning, stacklevel=2)
-from tcod import *
+
+warnings.warn(
+    "'import tcod as libtcodpy' is preferred.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+from tcod import *  # noqa: F4
