4d36258eea414768e547f52d7d5646dcab6460ff
Kyle Stewart <4b796c65@gmail.com>
2019-05-31 12:00:59 -0700

    Update threaded jobs example.

--- examples/thread_jobs.py
+++ examples/thread_jobs.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
+# To the extent possible under law, the libtcod maintainers have waived all
+# copyright and related or neighboring rights for this example.  This work is
+# published from: United States.
+# https://creativecommons.org/publicdomain/zero/1.0/
+"""A parallelization example for the field-of-view and path-finding tasks.
 
+Because of the Python GIL you can have only one thread in Python at a time.
+However, many C functions with long computations will release the GIL for their
+duration, allowing another Python thread to call into another C function.
+
+This script tests the viability of running python-tcod tasks in parallel.
+Typically the field-of-view tasks run good but not great, and the path-finding
+tasks run poorly.
+"""
 import sys
 
+import concurrent.futures
 import multiprocessing
 import platform
-import threading
-try:
-    import Queue as queue
-except ImportError:
-    import queue
 import timeit
+from typing import List, Tuple, Callable
 
 import tcod
-#import libtcodpy as tcod
+
 
 THREADS = multiprocessing.cpu_count()
 
@@ -20,85 +30,96 @@ MAP_WIDTH = 100
 MAP_HEIGHT = 100
 MAP_NUMBER = 500
 
-PATH_NUMBER = 500
+REPEAT = 10  # Number to times to run a test. Only the fastest result is shown.
 
-class JobConsumer(threading.Thread):
 
-    def __init__(self, i):
-        threading.Thread.__init__(self)
-        self.daemon = True
-        self.astar = tcod.path_new_using_map(maps[i])
+def test_fov(map_: tcod.map.Map) -> tcod.map.Map:
+    map_.compute_fov(MAP_WIDTH // 2, MAP_HEIGHT // 2)
+    return map_
 
-    def run(self):
-        while 1:
-            job, obj = jobs.get()
-            if job == 'fov':
-                tcod.map_compute_fov(obj, MAP_WIDTH // 2, MAP_HEIGHT // 2)
-            elif job == 'astar':
-                tcod.path_compute(self.astar,
-                                  0, 0, MAP_WIDTH - 1 , MAP_HEIGHT - 1)
-                x, y = tcod.path_walk(self.astar, False)
-                while x is not None:
-                    x, y = tcod.path_walk(self.astar, False)
-            jobs.task_done()
 
-maps = [tcod.map_new(MAP_WIDTH, MAP_HEIGHT) for i in range(MAP_NUMBER)]
-jobs = queue.Queue()
-threads = [JobConsumer(i) for i in range(THREADS)]
+def test_fov_single(maps: List[tcod.map.Map]) -> None:
+    for map_ in maps:
+        test_fov(map_)
 
-def test_fov_single():
-    for m in maps:
-        tcod.map_compute_fov(m, MAP_WIDTH // 2, MAP_HEIGHT // 2)
 
-def test_fov_threads():
-    for m in maps:
-        jobs.put(('fov', m))
-    jobs.join()
+def test_fov_threads(
+    executor: concurrent.futures.Executor, maps: List[tcod.map.Map]
+) -> None:
+    for result in executor.map(test_fov, maps):
+        pass
 
-def test_astar_single():
-    astar = tcod.path_new_using_map(maps[0])
-    for _ in range(PATH_NUMBER):
-        tcod.path_compute(astar, 0, 0, MAP_WIDTH - 1 , MAP_HEIGHT - 1)
-        x, y = tcod.path_walk(astar, False)
-        while x is not None:
-            x, y = tcod.path_walk(astar, False)
 
-def test_astar_threads():
-    for _ in range(PATH_NUMBER):
-        jobs.put(('astar', None))
-    jobs.join()
+def test_astar(map_: tcod.map.Map) -> List[Tuple[int, int]]:
+    astar = tcod.path.AStar(map_)
+    return astar.get_path(0, 0, MAP_WIDTH - 1, MAP_HEIGHT - 1)
 
-def main():
-    for m in maps:
-        for y in range(MAP_HEIGHT):
-            for x in range(MAP_WIDTH):
-                tcod.map_set_properties(m, x, y, True, True)
 
-    for thread in threads:
-        thread.start()
+def test_astar_single(maps: List[tcod.map.Map]) -> None:
+    for map_ in maps:
+        test_astar(map_)
 
-    print('Python %s\n%s\n%s' % (sys.version, platform.platform(),
-                                   platform.processor()))
 
-    print('\nComputing field-of-view for %i empty %ix%i maps.' %
-          (len(maps), MAP_WIDTH, MAP_HEIGHT))
-    single_time = min(timeit.repeat(test_fov_single, number=1))
-    print('1 thread: %.2fms' % (single_time * 1000))
+def test_astar_threads(
+    executor: concurrent.futures.Executor, maps: List[tcod.map.Map]
+) -> None:
+    for result in executor.map(test_astar, maps):
+        pass
 
-    multi_time = min(timeit.repeat(test_fov_threads, number=1))
-    print('%i threads: %.2fms' % (THREADS, multi_time * 1000))
-    print('%.2f%% efficiency' %
-          (single_time / (multi_time * THREADS) * 100))
 
-    print('\nComputing AStar from corner to corner %i times on seperate empty'
-          ' %ix%i maps.' % (PATH_NUMBER, MAP_WIDTH, MAP_HEIGHT))
-    single_time = min(timeit.repeat(test_astar_single, number=1))
-    print('1 thread: %.2fms' % (single_time * 1000))
+def run_test(
+    maps: List[tcod.map.Map],
+    single_func: Callable[[List[tcod.map.Map]], None],
+    multi_func: Callable[
+        [concurrent.futures.Executor, List[tcod.map.Map]], None
+    ],
+) -> None:
+    """Run a function designed for a single thread and compare it to a threaded
+    version.
 
-    multi_time = min(timeit.repeat(test_astar_threads, number=1))
-    print('%i threads: %.2fms' % (THREADS, multi_time * 1000))
-    print('%.2f%% efficiency' %
-          (single_time / (multi_time * THREADS) * 100))
+    This prints the results of these tests.
+    """
+    single_time = min(
+        timeit.repeat(lambda: single_func(maps), number=1, repeat=REPEAT)
+    )
+    print(f"Single threaded: {single_time * 1000:.2f}ms")
 
-if __name__ == '__main__':
+    for i in range(1, THREADS + 1):
+        executor = concurrent.futures.ThreadPoolExecutor(i)
+        multi_time = min(
+            timeit.repeat(
+                lambda: multi_func(executor, maps), number=1, repeat=REPEAT
+            )
+        )
+        print(
+            f"{i} threads: {multi_time * 1000:.2f}ms, "
+            f"{single_time / (multi_time * i) * 100:.2f}% efficiency"
+        )
+
+
+def main() -> None:
+    """Setup and run tests."""
+    maps = [tcod.map.Map(MAP_WIDTH, MAP_HEIGHT) for i in range(MAP_NUMBER)]
+    for map_ in maps:
+        map_.walkable[...] = True
+        map_.transparent[...] = True
+
+    print(
+        f"Python {sys.version}\n{platform.platform()}\n{platform.processor()}"
+    )
+
+    print(
+        f"\nComputing field-of-view for "
+        f"{len(maps)} empty {MAP_WIDTH}x{MAP_HEIGHT} maps."
+    )
+    run_test(maps, test_fov_single, test_fov_threads)
+
+    print(
+        f"\nComputing AStar from corner to corner {len(maps)} times "
+        f"on separate empty {MAP_WIDTH}x{MAP_HEIGHT} maps."
+    )
+    run_test(maps, test_astar_single, test_astar_threads)
+
+
+if __name__ == "__main__":
     main()
