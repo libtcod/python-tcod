ddf26f71b2809e3a531139863c0d0b298284dc52
Kyle Stewart <4b796c65@gmail.com>
2020-06-12 19:29:51 -0700

    Change TravisCI to use Ubuntu Bionic environment.
    Use the system install of SDL when it's up-to-date.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    export DISPLAY=:99.0
-    sh -e /etc/init.d/xvfb start
+    SDL_MAJOR=2
+    SDL_MINOR=5
+    UPDATE_SDL=true
 
-    # Update SDL2 to a recent version.
-    wget -O - https://www.libsdl.org/release/SDL2-2.0.8.tar.gz | tar xz
-    (cd SDL2-* && ./configure --prefix=$HOME/.local && make -j 3 install)
-    PATH=~/.local/bin:$PATH
+    if [[ $(sdl2-config --version) =~ ([0-9]*).([0-9]*).([0-9]*) ]]; then
+        if (( ${BASH_REMATCH[1]} >= $SDL_MAJOR && ${BASH_REMATCH[3]} >= $SDL_MINOR )); then
+            UPDATE_SDL=false
+        fi
+    fi
+
+    if [[ $UPDATE_SDL == "true" ]]; then
+        # Update SDL2 to a recent version.
+        wget -O - https://www.libsdl.org/release/SDL2-2.0.8.tar.gz | tar xz
+        (cd SDL2-* && ./configure --prefix=$HOME/.local && make -j 3 install)
+        PATH=~/.local/bin:$PATH
+    fi
 fi
