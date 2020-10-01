88c56dcdc7f93f55d7858e4906f49006fbfbcd77
Kyle Stewart <4b796c65+github@gmail.com>
2020-10-01 05:12:33 -0700

    Update PyPy jobs.
    Backported changes from python-esdl.

--- .appveyor/install_python.ps1
+++ .appveyor/install_python.ps1
@@ -10,7 +10,7 @@ if ($env:PYPY -or $env:PYPY3) {
     $env:PYPY = $env:PYPY + '-win32'
     $env:PYTHON = 'C:\' + $env:PYPY + '\' + $env:PYPY_EXE
     $env:PATH += ';' + 'C:\' + $env:PYPY + '\'
-    $PYPY_DOWNLOAD = 'https://bitbucket.org/pypy/pypy/downloads/' + $env:PYPY + '.zip'
+    $PYPY_DOWNLOAD = 'https://downloads.python.org/pypy/' + $env:PYPY + '.zip'
     Invoke-WebRequest $PYPY_DOWNLOAD -OutFile C:\pypy.zip
     & '7z' x C:\pypy.zip -oC:\
     & $env:PYTHON -m ensurepip
@@ -22,6 +22,6 @@ if ($env:WEB_PYTHON) {
     $env:PYTHON = 'C:\UserPython\python.exe'
 }
 & $env:PYTHON -m pip install --disable-pip-version-check --no-warn-script-location "virtualenv>=20"
-& $env:PYTHON -m virtualenv --download venv
+& $env:PYTHON -m virtualenv venv
 
 if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
