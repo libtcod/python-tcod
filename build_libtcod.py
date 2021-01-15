16900814a0f888f8b85dd57e7a95f0322952ec97
Kyle Benesch <4b796c65+github@gmail.com>
2021-01-14 23:06:49 -0800

    Test experimental branch of delocate.
    Update MacOS to use SDL 2.0.14, a version known to have issues with
    delocate.

--- build_libtcod.py
+++ build_libtcod.py
@@ -27,9 +27,7 @@ SDL2_PARSE_VERSION = os.environ.get("SDL_VERSION", "2.0.5")
 # The SDL2 version to include in binary distributions.
 # MacOS delocate-wheel will fail without throwing an error when bundling
 # modern versions of SDL.
-SDL2_BUNDLE_VERSION = os.environ.get(
-    "SDL_VERSION", "2.0.10" if sys.platform == "darwin" else "2.0.14"
-)
+SDL2_BUNDLE_VERSION = os.environ.get("SDL_VERSION", "2.0.14")
 
 HEADER_PARSE_PATHS = ("tcod/", "libtcod/src/libtcod/")
 HEADER_PARSE_EXCLUDES = ("gl2_ext_.h", "renderer_gl_internal.h", "event.h")
