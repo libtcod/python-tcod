12d0e114b3fbfacea1782c8361050c0169d59033
Kyle Benesch <4b796c65+github@gmail.com>
2021-03-31 14:14:03 -0700

    Update TTF example.

--- examples/ttf.py
+++ examples/ttf.py
@@ -4,9 +4,10 @@
 You will need to get this external library from PyPI:
 
     pip install freetype-py
-
-This script has known issues and may crash when the window is resized.
 """
+# To the extent possible under law, the libtcod maintainers have waived all
+# copyright and related or neighboring rights to this example script.
+# https://creativecommons.org/publicdomain/zero/1.0/
 from typing import Tuple
 
 import freetype  # type: ignore  # pip install freetype-py
@@ -17,13 +18,21 @@ FONT = "VeraMono.ttf"
 
 
 def load_ttf(path: str, size: Tuple[int, int]) -> tcod.tileset.Tileset:
-    """Load a TTF file as a tcod tileset."""
+    """Load a TTF file and return a tcod Tileset.
+
+    `path` is the file path to the font, this can be any font supported by the
+    FreeType library.
+
+    `size` is the (width, height) of the Tileset in pixels.
+
+    Feel free to use this function in your own code.
+    """
     ttf = freetype.Face(path)
     ttf.set_pixel_sizes(*size)
-    half_advance = size[0] - (ttf.bbox.xMax - ttf.bbox.xMin) // 64
 
     tileset = tcod.tileset.Tileset(*size)
     for codepoint, glyph_index in ttf.get_chars():
+        # Add every glyph to the Tileset.
         ttf.load_glyph(glyph_index)
         bitmap = ttf.glyph.bitmap
         assert bitmap.pixel_mode == freetype.FT_PIXEL_MODE_GRAY
@@ -31,15 +40,20 @@ def load_ttf(path: str, size: Tuple[int, int]) -> tcod.tileset.Tileset:
             (bitmap.width, bitmap.rows), order="F"
         )
         if bitmap_array.size == 0:
-            continue
+            continue  # Skip blank glyphs.
         output_image = np.zeros(size, dtype=np.uint8, order="F")
         out_slice = output_image
-        left = ttf.glyph.bitmap_left + half_advance
-        top = size[1] - ttf.glyph.bitmap_top + ttf.bbox.yMin // 64
+
+        # Adjust the position to center this glyph on the tile.
+        left = (size[0] - bitmap.width) // 2
+        top = size[1] - ttf.glyph.bitmap_top + ttf.size.descender // 64
+
+        # `max` is used because I was too lazy to properly slice the array.
         out_slice = out_slice[max(0, left) :, max(0, top) :]
         out_slice[
             : bitmap_array.shape[0], : bitmap_array.shape[1]
         ] = bitmap_array[: out_slice.shape[0], : out_slice.shape[1]]
+
         tileset.set_tile(codepoint, output_image.transpose())
     return tileset
 
@@ -53,8 +67,10 @@ def main() -> None:
     ) as context:
         while True:
             console.clear()
+            # Draw checkerboard.
             console.tiles_rgb["bg"][::2, ::2] = 0x20
             console.tiles_rgb["bg"][1::2, 1::2] = 0x20
+            # Print ASCII characters.
             console.tiles_rgb["ch"][:16, :6] = np.arange(0x20, 0x80).reshape(
                 0x10, -1, order="F"
             )
@@ -67,6 +83,7 @@ def main() -> None:
                     isinstance(event, tcod.event.WindowResized)
                     and event.type == "WINDOWSIZECHANGED"
                 ):
+                    # Resize the Tileset to match the new screen size.
                     context.change_tileset(
                         load_ttf(
                             path=FONT,
