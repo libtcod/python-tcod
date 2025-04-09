ba3bb24882e9571cffab9441b93bef261088408d
Kyle Benesch <4b796c65+github@gmail.com>
2025-04-08 19:36:16 -0700

    Upgrade to use SDL3 version of libtcod

--- build_sdl.py
+++ build_sdl.py
@@ -12,6 +12,7 @@ import subprocess
 import sys
 import zipfile
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Any
 
 import pcpp  # type: ignore[import-untyped]
@@ -23,11 +24,11 @@ import requests
 BIT_SIZE, LINKAGE = platform.architecture()
 
 # Reject versions of SDL older than this, update the requirements in the readme if you change this.
-SDL_MIN_VERSION = (2, 0, 10)
-# The SDL2 version to parse and export symbols from.
-SDL2_PARSE_VERSION = os.environ.get("SDL_VERSION", "2.0.20")
-# The SDL2 version to include in binary distributions.
-SDL2_BUNDLE_VERSION = os.environ.get("SDL_VERSION", "2.28.1")
+SDL_MIN_VERSION = (3, 2, 0)
+# The SDL version to parse and export symbols from.
+SDL_PARSE_VERSION = os.environ.get("SDL_VERSION", "3.2.10")
+# The SDL version to include in binary distributions.
+SDL_BUNDLE_VERSION = os.environ.get("SDL_VERSION", "3.2.10")
 
 
 # Used to remove excessive newlines in debug outputs.
@@ -41,7 +42,7 @@ RE_PIXELFORMAT = re.compile(r"(?P<name>SDL_PIXELFORMAT_\w+) =[^,}]*")
 # Most SDLK names need their values scrubbed.
 RE_SDLK = re.compile(r"(?P<name>SDLK_\w+) =.*?(?=,\n|}\n)")
 # Remove compile time assertions from the cdef.
-RE_ASSERT = re.compile(r"^.*SDL_compile_time_assert.*$", re.MULTILINE)
+RE_ASSERT = re.compile(r"^.*SDL_COMPILE_TIME_ASSERT.*$", re.MULTILINE)
 # Padding values need to be scrubbed.
 RE_PADDING = re.compile(r"padding\[[^;]*\];")
 
@@ -63,6 +64,9 @@ IGNORE_DEFINES = frozenset(
         "SDL_HAS_FALLTHROUGH",
         "SDL_NO_THREAD_SAFETY_ANALYSIS",
         "SDL_SCOPED_CAPABILITY",
+        "SDL_NODISCARD",
+        "SDL_NOLONGLONG",
+        "SDL_WINAPI_FAMILY_PHONE",
         # Might show up in parsing and not in source.
         "SDL_ANDROID_EXTERNAL_STORAGE_READ",
         "SDL_ANDROID_EXTERNAL_STORAGE_WRITE",
@@ -82,16 +86,18 @@ IGNORE_DEFINES = frozenset(
 
 
 def check_sdl_version() -> None:
-    """Check the local SDL version on Linux distributions."""
+    """Check the local SDL3 version on Linux distributions."""
     if not sys.platform.startswith("linux"):
         return
     needed_version = f"{SDL_MIN_VERSION[0]}.{SDL_MIN_VERSION[1]}.{SDL_MIN_VERSION[2]}"
     try:
-        sdl_version_str = subprocess.check_output(["sdl2-config", "--version"], universal_newlines=True).strip()
+        sdl_version_str = subprocess.check_output(
+            ["pkg-config", "sdl3", "--modversion"], universal_newlines=True
+        ).strip()
     except FileNotFoundError as exc:
         msg = (
-            "libsdl2-dev or equivalent must be installed on your system and must be at least version"
-            f" {needed_version}.\nsdl2-config must be on PATH."
+            "libsdl3-dev or equivalent must be installed on your system and must be at least version"
+            f" {needed_version}.\nsdl3-config must be on PATH."
         )
         raise RuntimeError(msg) from exc
     print(f"Found SDL {sdl_version_str}.")
@@ -101,44 +107,44 @@ def check_sdl_version() -> None:
         raise RuntimeError(msg)
 
 
-def get_sdl2_file(version: str) -> Path:
-    """Return a path to an SDL2 archive for the current platform.  The archive is downloaded if missing."""
+def get_sdl_file(version: str) -> Path:
+    """Return a path to an SDL3 archive for the current platform.  The archive is downloaded if missing."""
     if sys.platform == "win32":
-        sdl2_file = f"SDL2-devel-{version}-VC.zip"
+        sdl_archive = f"SDL3-devel-{version}-VC.zip"
     else:
         assert sys.platform == "darwin"
-        sdl2_file = f"SDL2-{version}.dmg"
-    sdl2_local_file = Path("dependencies", sdl2_file)
-    sdl2_remote_file = f"https://www.libsdl.org/release/{sdl2_file}"
-    if not sdl2_local_file.exists():
-        print(f"Downloading {sdl2_remote_file}")
+        sdl_archive = f"SDL3-{version}.dmg"
+    sdl_local_file = Path("dependencies", sdl_archive)
+    sdl_remote_url = f"https://www.libsdl.org/release/{sdl_archive}"
+    if not sdl_local_file.exists():
+        print(f"Downloading {sdl_remote_url}")
         Path("dependencies/").mkdir(parents=True, exist_ok=True)
-        with requests.get(sdl2_remote_file) as response:  # noqa: S113
+        with requests.get(sdl_remote_url) as response:  # noqa: S113
             response.raise_for_status()
-            sdl2_local_file.write_bytes(response.content)
-    return sdl2_local_file
+            sdl_local_file.write_bytes(response.content)
+    return sdl_local_file
 
 
-def unpack_sdl2(version: str) -> Path:
+def unpack_sdl(version: str) -> Path:
     """Return the path to an extracted SDL distribution.  Creates it if missing."""
-    sdl2_path = Path(f"dependencies/SDL2-{version}")
+    sdl_path = Path(f"dependencies/SDL3-{version}")
     if sys.platform == "darwin":
-        sdl2_dir = sdl2_path
-        sdl2_path /= "SDL2.framework"
-    if sdl2_path.exists():
-        return sdl2_path
-    sdl2_arc = get_sdl2_file(version)
-    print(f"Extracting {sdl2_arc}")
-    if sdl2_arc.suffix == ".zip":
-        with zipfile.ZipFile(sdl2_arc) as zf:
+        sdl_dir = sdl_path
+        sdl_path /= "SDL3.framework"
+    if sdl_path.exists():
+        return sdl_path
+    sdl_archive = get_sdl_file(version)
+    print(f"Extracting {sdl_archive}")
+    if sdl_archive.suffix == ".zip":
+        with zipfile.ZipFile(sdl_archive) as zf:
             zf.extractall("dependencies/")
     elif sys.platform == "darwin":
-        assert sdl2_arc.suffix == ".dmg"
-        subprocess.check_call(["hdiutil", "mount", sdl2_arc])
-        subprocess.check_call(["mkdir", "-p", sdl2_dir])
-        subprocess.check_call(["cp", "-r", "/Volumes/SDL2/SDL2.framework", sdl2_dir])
-        subprocess.check_call(["hdiutil", "unmount", "/Volumes/SDL2"])
-    return sdl2_path
+        assert sdl_archive.suffix == ".dmg"
+        subprocess.check_call(["hdiutil", "mount", sdl_archive])
+        subprocess.check_call(["mkdir", "-p", sdl_dir])
+        subprocess.check_call(["cp", "-r", "/Volumes/SDL3/SDL3.xcframework/macos-arm64_x86_64/SDL3.framework", sdl_dir])
+        subprocess.check_call(["hdiutil", "unmount", "/Volumes/SDL3"])
+    return sdl_path
 
 
 class SDLParser(pcpp.Preprocessor):  # type: ignore[misc]
@@ -180,11 +186,9 @@ class SDLParser(pcpp.Preprocessor):  # type: ignore[misc]
             return False  # Value is a floating point number.
         if tokens[0].value.startswith("SDL_PR") and (tokens[0].value.endswith("32") or tokens[0].value.endswith("64")):
             return False  # Data type for printing, which is not needed.
-        return bool(
-            tokens[0].value.startswith("KMOD_")
-            or tokens[0].value.startswith("SDL_")
-            or tokens[0].value.startswith("AUDIO_")
-        )
+        if tokens[0].value.startswith("SDL_PLATFORM_"):
+            return False  # Ignore platform definitions
+        return bool(tokens[0].value.startswith("SDL_"))
 
     def on_directive_handle(
         self,
@@ -197,6 +201,8 @@ class SDLParser(pcpp.Preprocessor):  # type: ignore[misc]
         if directive.value == "define" and self._should_track_define(tokens):
             if tokens[2].type == "CPP_STRING":
                 self.known_string_defines[tokens[0].value] = tokens[2].value
+            elif tokens[2].value in self.known_string_defines:
+                self.known_string_defines[tokens[0].value] = "..."
             else:
                 self.known_defines.add(tokens[0].value)
         return super().on_directive_handle(directive, tokens, if_passthru, preceding_tokens)
@@ -205,25 +211,25 @@ class SDLParser(pcpp.Preprocessor):  # type: ignore[misc]
 check_sdl_version()
 
 if sys.platform == "win32" or sys.platform == "darwin":
-    SDL2_PARSE_PATH = unpack_sdl2(SDL2_PARSE_VERSION)
-    SDL2_BUNDLE_PATH = unpack_sdl2(SDL2_BUNDLE_VERSION)
+    SDL_PARSE_PATH = unpack_sdl(SDL_PARSE_VERSION)
+    SDL_BUNDLE_PATH = unpack_sdl(SDL_BUNDLE_VERSION)
 
-SDL2_INCLUDE: Path
+SDL_INCLUDE: Path
 if sys.platform == "win32":
-    SDL2_INCLUDE = SDL2_PARSE_PATH / "include"
+    SDL_INCLUDE = SDL_PARSE_PATH / "include"
 elif sys.platform == "darwin":
-    SDL2_INCLUDE = SDL2_PARSE_PATH / "Versions/A/Headers"
+    SDL_INCLUDE = SDL_PARSE_PATH / "Versions/A/Headers"
 else:  # Unix
     matches = re.findall(
         r"-I(\S+)",
-        subprocess.check_output(["sdl2-config", "--cflags"], universal_newlines=True),
+        subprocess.check_output(["pkg-config", "sdl3", "--cflags"], universal_newlines=True),
     )
     assert matches
 
     for match in matches:
-        if Path(match, "SDL_stdinc.h").is_file():
-            SDL2_INCLUDE = match
-    assert SDL2_INCLUDE
+        if Path(match, "SDL3/SDL_stdinc.h").is_file():
+            SDL_INCLUDE = match
+    assert SDL_INCLUDE
 
 
 EXTRA_CDEF = """
@@ -231,7 +237,7 @@ EXTRA_CDEF = """
 
 extern "Python" {
 // SDL_AudioCallback callback.
-void _sdl_audio_callback(void* userdata, Uint8* stream, int len);
+void _sdl_audio_callback(void* userdata, uint8_t* stream, int len);
 // SDL to Python log function.
 void _sdl_log_output_function(void *userdata, int category, SDL_LogPriority priority, const char *message);
 // Generic event watcher callback.
@@ -242,41 +248,45 @@ int _sdl_event_watcher(void* userdata, SDL_Event* event);
 
 def get_cdef() -> str:
     """Return the parsed code of SDL for CFFI."""
-    parser = SDLParser()
-    parser.add_path(SDL2_INCLUDE)
-    parser.parse(
+    with TemporaryDirectory() as temp_dir:
+        # Add a false SDL_oldnames.h to prevent old symbols from being collected
+        fake_header_dir = Path(temp_dir, "SDL3")
+        fake_header_dir.mkdir()
+        (fake_header_dir / "SDL_oldnames.h").write_text("")
+
+        parser = SDLParser()
+        parser.add_path(temp_dir)
+        parser.add_path(SDL_INCLUDE)
+        parser.parse(
+            """
+        // Remove extern keyword.
+        #define extern
+        // Ignore some SDL assert statements.
+        #define DOXYGEN_SHOULD_IGNORE_THIS
+        #define SDL_COMPILE_TIME_ASSERT(x, y)
+
+        #define _SIZE_T_DEFINED_
+        typedef int... size_t;
+
+        #define SDL_oldnames_h_
+
+        #include <SDL3/SDL.h>
         """
-    // Remove extern keyword.
-    #define extern
-    // Ignore some SDL assert statements.
-    #define DOXYGEN_SHOULD_IGNORE_THIS
+        )
+        sdl_cdef = parser.get_output()
 
-    #define _SIZE_T_DEFINED_
-    typedef int... size_t;
-
-    // Skip these headers.
-    #define SDL_atomic_h_
-    #define SDL_thread_h_
-
-    #include <SDL.h>
-    """
-    )
-    sdl2_cdef = parser.get_output()
-    sdl2_cdef = RE_VAFUNC.sub("", sdl2_cdef)
-    sdl2_cdef = RE_INLINE.sub("", sdl2_cdef)
-    sdl2_cdef = RE_PIXELFORMAT.sub(r"\g<name> = ...", sdl2_cdef)
-    sdl2_cdef = RE_SDLK.sub(r"\g<name> = ...", sdl2_cdef)
-    sdl2_cdef = RE_NEWLINES.sub("\n", sdl2_cdef)
-    sdl2_cdef = RE_ASSERT.sub("", sdl2_cdef)
-    sdl2_cdef = RE_PADDING.sub("padding[...];", sdl2_cdef)
-    sdl2_cdef = (
-        sdl2_cdef.replace("int SDL_main(int argc, char *argv[]);", "")
-        .replace("typedef unsigned int uintptr_t;", "typedef int... uintptr_t;")
-        .replace("typedef unsigned int size_t;", "typedef int... size_t;")
+    sdl_cdef = RE_VAFUNC.sub("", sdl_cdef)
+    sdl_cdef = RE_INLINE.sub("", sdl_cdef)
+    sdl_cdef = RE_PIXELFORMAT.sub(r"\g<name> = ...", sdl_cdef)
+    sdl_cdef = RE_SDLK.sub(r"\g<name> = ...", sdl_cdef)
+    sdl_cdef = RE_NEWLINES.sub("\n", sdl_cdef)
+    sdl_cdef = RE_PADDING.sub("padding[...];", sdl_cdef)
+    sdl_cdef = sdl_cdef.replace("typedef unsigned int uintptr_t;", "typedef int... uintptr_t;").replace(
+        "typedef unsigned int size_t;", "typedef int... size_t;"
     )
     for name in FLEXIBLE_STRUCTS:
-        sdl2_cdef = sdl2_cdef.replace(f"}} {name};", f"...;}} {name};")
-    return sdl2_cdef + EXTRA_CDEF
+        sdl_cdef = sdl_cdef.replace(f"}} {name};", f"...;}} {name};")
+    return sdl_cdef + EXTRA_CDEF
 
 
 include_dirs: list[str] = []
@@ -288,33 +298,37 @@ library_dirs: list[str] = []
 
 
 if sys.platform == "darwin":
-    extra_link_args += ["-framework", "SDL2"]
+    extra_link_args += ["-framework", "SDL3"]
 else:
-    libraries += ["SDL2"]
+    libraries += ["SDL3"]
 
-# Bundle the Windows SDL2 DLL.
+# Bundle the Windows SDL DLL.
 if sys.platform == "win32":
-    include_dirs.append(str(SDL2_INCLUDE))
+    include_dirs.append(str(SDL_INCLUDE))
     ARCH_MAPPING = {"32bit": "x86", "64bit": "x64"}
-    SDL2_LIB_DIR = Path(SDL2_BUNDLE_PATH, "lib/", ARCH_MAPPING[BIT_SIZE])
-    library_dirs.append(str(SDL2_LIB_DIR))
-    SDL2_LIB_DEST = Path("tcod", ARCH_MAPPING[BIT_SIZE])
-    SDL2_LIB_DEST.mkdir(exist_ok=True)
-    SDL2_LIB_DEST_FILE = SDL2_LIB_DEST / "SDL2.dll"
-    SDL2_LIB_FILE = SDL2_LIB_DIR / "SDL2.dll"
-    if not SDL2_LIB_DEST_FILE.exists() or SDL2_LIB_FILE.read_bytes() != SDL2_LIB_DEST_FILE.read_bytes():
-        shutil.copy(SDL2_LIB_FILE, SDL2_LIB_DEST_FILE)
+    SDL_LIB_DIR = Path(SDL_BUNDLE_PATH, "lib/", ARCH_MAPPING[BIT_SIZE])
+    library_dirs.append(str(SDL_LIB_DIR))
+    SDL_LIB_DEST = Path("tcod", ARCH_MAPPING[BIT_SIZE])
+    SDL_LIB_DEST.mkdir(exist_ok=True)
+    SDL_LIB_DEST_FILE = SDL_LIB_DEST / "SDL3.dll"
+    SDL_LIB_FILE = SDL_LIB_DIR / "SDL3.dll"
+    if not SDL_LIB_DEST_FILE.exists() or SDL_LIB_FILE.read_bytes() != SDL_LIB_DEST_FILE.read_bytes():
+        shutil.copy(SDL_LIB_FILE, SDL_LIB_DEST_FILE)
 
-# Link to the SDL2 framework on MacOS.
+# Link to the SDL framework on MacOS.
 # Delocate will bundle the binaries in a later step.
 if sys.platform == "darwin":
-    HEADER_DIR = Path(SDL2_PARSE_PATH, "Headers")
+    HEADER_DIR = Path(SDL_PARSE_PATH, "Headers")
     include_dirs.append(HEADER_DIR)
-    extra_link_args += [f"-F{SDL2_BUNDLE_PATH}/.."]
-    extra_link_args += ["-rpath", f"{SDL2_BUNDLE_PATH}/.."]
+    extra_link_args += [f"-F{SDL_BUNDLE_PATH}/.."]
+    extra_link_args += ["-rpath", f"{SDL_BUNDLE_PATH}/.."]
     extra_link_args += ["-rpath", "/usr/local/opt/llvm/lib/"]
 
-# Use sdl2-config to link to SDL2 on Linux.
+# Use sdl-config to link to SDL on Linux.
 if sys.platform not in ["win32", "darwin"]:
-    extra_compile_args += subprocess.check_output(["sdl2-config", "--cflags"], universal_newlines=True).strip().split()
-    extra_link_args += subprocess.check_output(["sdl2-config", "--libs"], universal_newlines=True).strip().split()
+    extra_compile_args += (
+        subprocess.check_output(["pkg-config", "sdl3", "--cflags"], universal_newlines=True).strip().split()
+    )
+    extra_link_args += (
+        subprocess.check_output(["pkg-config", "sdl3", "--libs"], universal_newlines=True).strip().split()
+    )
